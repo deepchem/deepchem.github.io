1894d8c3ea2f29bd8d52d5adaaa8998defe06fd0
kid-116 <mehultodi116@gmail.com>
2022-12-03 23:12:53 +0530

    Add favicon

--- deepchem/pages/_app.js
+++ deepchem/pages/_app.js
@@ -1,12 +1,18 @@
-import "../styles/globals.css";
-import Layout from "../components/layout";
+import Head from "next/head";
 
-//Styles for carousel
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import "../styles/globals.css";
+import Layout from "../layouts/layout";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>DeepChem</title>
+        
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
