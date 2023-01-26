83364cc18bcd8ad2e8bf2ae3acf5394aeca51791
Dibyam <dibyam1101@gmail.com>
2023-01-26 21:35:07 +0530

    Add custom _app.js and _document.js

---
+++ new-website/deepchem/pages/_app.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import "../styles/globals.css";
+import Layout from "../layouts/layout";
+
+import "../components/CustomCarousel/CustomCarousel.styles.css";
+
+import "react-tooltip/dist/react-tooltip.css";
+
+/**
+ * Custom _app.js to render common layouts for all pages
+ * @component
+ * @param {Object} props - Props passed to the component
+ * @param {React.Component} props.Component - Component to be rendered within the common layout
+ * @param {Object} props.pageProps - Props passed to the child component
+ * @return {JSX} - JSX for the App component
+ */
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>DeepChem</title>
+
+        <link rel="shortcut icon" href="/deepchem.github.io/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+};
+
+export default MyApp;
