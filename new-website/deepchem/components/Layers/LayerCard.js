304c7e1ffcd132d508ada189e728b8f8293406fe
Dibyam Kumar <dibyam1101@gmail.com>
2023-11-23 12:44:41 +0530

    Fix lint errors

--- new-website/deepchem/components/Layers/LayerCard.js
+++ new-website/deepchem/components/Layers/LayerCard.js
@@ -11,49 +11,47 @@ import deepchemKeras from "../../public/icons/deepchem-keras.png";
  * @return {string} - The parsed and formatted string
  */
 function parseName(name) {
-    name = name.replaceAll(/([A-Z]+)/g, " $1");
-    name = name.replace(/([^ ])(Layer)/, "$1 Layer");
-    return name;
-  }
+  name = name.replaceAll(/([A-Z]+)/g, " $1");
+  name = name.replace(/([^ ])(Layer)/, "$1 Layer");
+  return name;
+}
 
 const LayerCard = ({ layer }) => {
-    let models = layer.models.length
-        ? layer.models.join(", ")
-        : "N/A";
-    models = models + " " + "\xa0".repeat(300);
+  let models = layer.models.length ? layer.models.join(", ") : "N/A";
+  models = models + " " + "\xa0".repeat(300);
 
-    return (
-        <>
-          <Link href={layer.url} target="_blank">
-            <div className="flex flex-col gap-4 py-4 px-5 bg-white shadow-[0_4px_4px_rgba(0,0,0,0.25)] rounded-[10px] layer-card hover:scale-[1.03] transition-all">
-                <div className="flex flex-row justify-between w-full gap-8 items-start">
-                    <div className="text-xl font-medium text-dc-orange">
-                        {parseName(layer.name)}
-                    </div>
-                    <div className="flex flex-row items-center gap-1.5 bg-dc-light-blue/5 px-2 py-1 rounded-md">
-                        {layer.category === "torch" && (
-                            <Image src={deepchemPytorch} alt="PyTorch" width={16} />
-                        )}
-                        {layer.category === "keras" && (
-                            <Image src={deepchemKeras} alt="Keras" width={16} />
-                        )}
-                    </div>
-                    <div className="font-medium text-sm text-dc-gray">
-                        {layer.category}
-                    </div>
-                </div>
-                {
-                    <div className="text-base font-medium text-dc-light-blue w-full mt-4 md:mt-auto">
-                    <p className="text-dc-gray/60">Acceptable Models</p>
-                    <p className="text-xs text-dc-gray font-medium break-all">
-                        {models}
-                    </p>
-                    </div>
-                }
+  return (
+    <>
+      <Link href={layer.url} target="_blank">
+        <div className="flex flex-col gap-4 py-4 px-5 bg-white shadow-[0_4px_4px_rgba(0,0,0,0.25)] rounded-[10px] layer-card hover:scale-[1.03] transition-all">
+          <div className="flex flex-row justify-between w-full gap-8 items-start">
+            <div className="text-xl font-medium text-dc-orange">
+              {parseName(layer.name)}
             </div>
-          </Link>
-        </>
-    );
+            <div className="flex flex-row items-center gap-1.5 bg-dc-light-blue/5 px-2 py-1 rounded-md">
+              {layer.category === "torch" && (
+                <Image src={deepchemPytorch} alt="PyTorch" width={16} />
+              )}
+              {layer.category === "keras" && (
+                <Image src={deepchemKeras} alt="Keras" width={16} />
+              )}
+            </div>
+            <div className="font-medium text-sm text-dc-gray">
+              {layer.category}
+            </div>
+          </div>
+          {
+            <div className="text-base font-medium text-dc-light-blue w-full mt-4 md:mt-auto">
+              <p className="text-dc-gray/60">Acceptable Models</p>
+              <p className="text-xs text-dc-gray font-medium break-all">
+                {models}
+              </p>
+            </div>
+          }
+        </div>
+      </Link>
+    </>
+  );
 };
 
 export default LayerCard;
--- new-website/deepchem/pages/layers.js
+++ new-website/deepchem/pages/layers.js
@@ -8,18 +8,15 @@ import FilterButton from "/components/common/FilterButton";
 import layers from "/data/layers/layers.json";
 import modelList from "/data/layers/models.json";
 
-import deepchemPyTorch from "/public/icons/deepchem-pytorch.png";
-import deepchemKeras from "/public/icons/deepchem-keras.png";
 import deepchemFilter from "/public/icons/deepchem-filter.png";
-
 
 /**
  * Models component that displays the models page of the application
  * @component
  * @return {JSX.Element} The JSX element to render the Model component
  */
-const Layers = () => { 
 
+const Layers = () => {
   const [filteredLayers, setFilteredLayers] = useState(layers);
   const [models, setModels] = useState([]);
   const [isPopUp, setIsPopUp] = useState(false);
@@ -43,9 +40,7 @@ const Layers = () => {
   useEffect(() => {
     let newlayers = [];
     const flayers = layers;
-    if (
-      models.length === 0
-    ) {
+    if (models.length === 0) {
       newlayers = layers;
     } else {
       flayers.map((flayer) => {
@@ -73,7 +68,6 @@ const Layers = () => {
   }, []);
 
   return (
-
     <div className="layers">
       <div
         className={`${
@@ -81,7 +75,7 @@ const Layers = () => {
         } fixed bg-dc-gray/80 w-full h-[100vh] top-0 lg:hidden`}
         onClick={handlePopUp}
       ></div>
-      <div className= "flex flex-col items-start w-full px-[25px] 2xl:px-[300px] py-8 lg:py-16 gap-6">
+      <div className="flex flex-col items-start w-full px-[25px] 2xl:px-[300px] py-8 lg:py-16 gap-6">
         {/* HEADING BEGIN */}
         <div className="flex flex-col w-[100%] justify-between py-2.5">
           <div className="lg:text-4xl text-[26px]">Our Layers</div>
@@ -90,41 +84,41 @@ const Layers = () => {
               <Image src={deepchemFilter} alt={"Filter Button"} width={18} />
             </button>
           </div>
-            {/* HEADING END */}
-            {/* BODY BEGIN */}
-            <div className="flex flex-col items-start gap-12 w-full">
-              {/* FILTER SECTION BEGIN */}
-              <div
-                className={`${
-                  isPopUp
-                    ? "fixed flex left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 px-8 py-6 bg-white shadow-[0_4px_4px_rgba(0,0,0,0.25)] rounded-[10px] w-[89%] overflow-y-auto h-[85vh]"
-                    : "hidden"
-                } lg:flex lg:relative lg:left-0 lg:top-0 lg:translate-x-0 lg:translate-y-0 lg:shadow-none lg:rounded-none flex-col items-start gap-5 lg:min-w-[240px] lg:max-w-[240px] lg:border-r-2 lg:py-0 lg:pl-0 pr-4 lg:border-dc-light-gray`}
-              ></div>
-              {/* MODEL BEGIN */}
-              <div className="category-filter">
-                <div className="category-text-filter">Model</div>
-                <div className="btn-container-filter">
-                  {modelList.map((model, index) => (
-                    <div key={`feat-${index}`}>
-                      <button
-                        className="rmv-filter"
-                        onClick={() => {
-                          handleClick("models", model);
-                        }}
-                      >
-                        <FilterButton
-                          category={models}
-                          name={model}
-                          image={null}
-                        />
-                      </button>
-                    </div>
-                  ))}
-                </div>
+          {/* HEADING END */}
+          {/* BODY BEGIN */}
+          <div className="flex flex-col items-start gap-12 w-full">
+            {/* FILTER SECTION BEGIN */}
+            <div
+              className={`${
+                isPopUp
+                  ? "fixed flex left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 px-8 py-6 bg-white shadow-[0_4px_4px_rgba(0,0,0,0.25)] rounded-[10px] w-[89%] overflow-y-auto h-[85vh]"
+                  : "hidden"
+              } lg:flex lg:relative lg:left-0 lg:top-0 lg:translate-x-0 lg:translate-y-0 lg:shadow-none lg:rounded-none flex-col items-start gap-5 lg:min-w-[240px] lg:max-w-[240px] lg:border-r-2 lg:py-0 lg:pl-0 pr-4 lg:border-dc-light-gray`}
+            ></div>
+            {/* MODEL BEGIN */}
+            <div className="category-filter">
+              <div className="category-text-filter">Model</div>
+              <div className="btn-container-filter">
+                {modelList.map((model, index) => (
+                  <div key={`feat-${index}`}>
+                    <button
+                      className="rmv-filter"
+                      onClick={() => {
+                        handleClick("models", model);
+                      }}
+                    >
+                      <FilterButton
+                        category={models}
+                        name={model}
+                        image={null}
+                      />
+                    </button>
+                  </div>
+                ))}
               </div>
-              {/* MODEL END */}
-              {/* LAYER CARDS SECTION BEGIN */}
+            </div>
+            {/* MODEL END */}
+            {/* LAYER CARDS SECTION BEGIN */}
             <div
               className={`items-start ${
                 filteredLayers.length
@@ -143,9 +137,9 @@ const Layers = () => {
                 </div>
               )}
             </div>
-            {/* MODEL CARDS SECTION END */}  
+            {/* MODEL CARDS SECTION END */}
           </div>
-          {/*BODY END*/}
+          {/* BODY END */}
         </div>
       </div>
     </div>
